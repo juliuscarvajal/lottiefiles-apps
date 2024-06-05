ad77344a80247064328fd8d783d647271cc47339
Julius Carvajal <juliuscarvajal21@gmail.com>
2024-06-05 19:31:05 +1000

    fix: server actions

--- apps/lottiefiles-apps/src/app/page.tsx
+++ apps/lottiefiles-apps/src/app/page.tsx
@@ -8,22 +8,30 @@ export default function Index() {
       <div className="wrapper">
         <div className="p-4">
           <form
-          // action={async (e: FormData) => {
-          //   'use server';
-          //   console.log(`>>> form`, e);
-          //   const res = await fetch(
-          //     'https://lottiefiles.com/api/search/get-animations?query=loading+dots&type=all&aep=false&sort=popular&page=1'
-          //   );
-          //   const data = await res.json();
-          //   console.log(`>>> data`, data?.data);
-          // }}
+            action={async (e: FormData) => {
+              'use server';
+              console.log(`>>> form`, e);
+              const res = await fetch(
+                'https://lottiefiles.com/api/search/get-animations?query=loading+dots&type=all&aep=false&sort=popular&page=1'
+              );
+              const data = await res.json();
+              console.log(`>>> data`, data?.data);
+            }}
           >
-            <Input
-              type="search"
-              placeholder="Search animations"
-              className="dark:bg-grey-700 text-black p-4 rounded-lg w-full"
-            />
-            <Button type="submit">Search</Button>
+            <div className="flex gap-2">
+              <Input
+                type="search"
+                name="search"
+                placeholder="Search animations"
+                className="dark:bg-gray-700 dark:text-white p-4 rounded-lg w-full"
+              />
+              <Button
+                type="submit"
+                className="bg-gray-900 px-4 py-2 rounded-lg"
+              >
+                Search
+              </Button>
+            </div>
           </form>
         </div>
         <div className="container grid grid-cols-3 gap-4 p-4">
