a631ac5ab8d2e1b3006da7913e97289d81c94c90
Julius Carvajal <juliuscarvajal21@gmail.com>
2024-06-06 10:40:13 +1000

    fix: gitignore sw.js

--- libs/components/src/BackNav.tsx
+++ libs/components/src/BackNav.tsx
@@ -7,7 +7,7 @@ export const BackNav = () => {
   const router = useRouter();
   return (
     <Button
-      className="bg-gray-700 px-4 py-2 rounded-lg"
+      className="bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded-lg"
       onClick={() => router.back()}
     >
       Go back
